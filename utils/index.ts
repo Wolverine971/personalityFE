f5a39f894e2031124e2affe598460b515070f417
david.t.wayne <david.t.wayne.ctr@usmc.mil>
2021-10-10 14:56:05 -0400

    fix time passed calculation

--- utils/index.ts
+++ utils/index.ts
@@ -30,28 +30,48 @@ export function getCommentFromData (esComment: any) {
 }
 
 export const msToTime = (time: any) => {
-  const newDate: any = new Date()
-  const date: any = new Date(time)
+  const newDate: any = new Date().getTime()
+  let date: any = new Date(time).getTime()
   // it just works ok, gosh :)
-  let s = newDate - date
+  const differenceInTime = newDate - date
 
-  const ms = s % 1000
-  s = (s - ms) / 1000
-  const secs = s % 60
-  s = (s - secs) / 60
-  const mins = s % 60
-  s = (s - mins) / 60
-  const hrs = s % 60
-  s = (s - hrs) / 24
-  const days = s % 24
-
-  if (days) {
-    return days + ' days ago'
-  } else if (hrs) {
-    return hrs + ' hours ago'
+  const differenceInDays = differenceInTime / (1000 * 3600 * 24)
+  if (differenceInDays < 1) {
+    const hours = differenceInDays * 24
+    console.log(hours)
+    const newHours = Math.floor(hours)
+    if (newHours < 1) {
+      console.log(new Date(time))
+      const minutes = Math.floor(hours * 60)
+      return minutes + ((minutes === 1) ? ' minute ago' : ' minutes ago')
+    } else {
+      const newHours = Math.floor(hours)
+      return newHours + ((newHours === 1) ? ' hour ago' : ' hours ago')
+    }
+  } else if (differenceInDays > 30) {
+    date = new Date(time)
+    return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`
   } else {
-    return mins + ' minutes ' + secs + ' seconds ago'
+    return differenceInDays + ' days ago'
   }
+
+  // const ms = s % 1000
+  // s = (s - ms) / 1000
+  // const secs = s % 60
+  // s = (s - secs) / 60
+  // const mins = s % 60
+  // s = (s - mins) / 60
+  // const hrs = s % 60
+  // s = (s - hrs) / 24
+  // const days = s % 24
+
+  // if (days) {
+  //   return days + ' days ago'
+  // } else if (hrs) {
+  //   return hrs + ' hours ago'
+  // } else {
+  //   return mins + ' minutes ' + secs + ' seconds ago'
+  // }
 }
 
 export const msToDate = (time: any) => {
